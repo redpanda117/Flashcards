805dd1a093bf1ab96b4fb3f48592e12300671697
Tina Nguyen <nguyentina2005@yahoo.com>
2017-07-16 01:52:31 -0400

    almost there
    It works everything works. only need the error code for the cloze constructor now.

--- main.js
+++ main.js
@@ -1,52 +1,50 @@
 var inquirer = require("inquirer");
-//getting the exported files data
+//getting the exported files data. The constrctors are in these files.
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
 
-
+// keep tack of current question and score wronge and right answers an current question.
+var currentBasicQuestion = 0;
+var currentClozeQuestion = 0;
+var correct = 0;
+var incorrect = 0;
+var totalQuestion = 0;
 //the enpty array that wil hold all of the basic cards.
 var basicQuestion = [];
+//making an empty arry to put all of the cloze questions in.
+var clozeQuestion = [];
 
 //a function to push the cards into basicQuestion array
- function makeBasicCard(front,back) {
-    var addBasicCard = new BasicCard(front,back);
+function makeBasicCard(front, back) {
+    var addBasicCard = new BasicCard(front, back);
     basicQuestion.push(addBasicCard);
 };
 
 //creating the basic cards
-makeBasicCard( "What superhero can climb walls and shoot webs?", "SpiderMan");
+makeBasicCard("What superhero can climb walls and shoot webs?", "SpiderMan");
 makeBasicCard("He has been seen running with an ant army.", "AntMan");
 makeBasicCard("This woman has a lasso that make people speak the truth.", "Wonder Woman");
 
 //testing for errors
 //console.log(basicQuestion);
 
-//making an empty arry to put all of the cloze questions in.
-var clozeQuestion = [];
 //a function that push the cards into the clozeQuestion
-var makeClozeCard = function(text,cloze){
-    var addClozeCard = new ClozeCard(text,cloze);
+var makeClozeCard = function (text, cloze) {
+    var addClozeCard = new ClozeCard(text, cloze);
     clozeQuestion.push(addClozeCard);
 }
 
-makeClozeCard("The Hulk turns green when he is angry","Hulk");
+makeClozeCard("The Hulk turns green when he is angry", "Hulk");
 makeClozeCard("Natasha Romanova has many aliases but the one most poeple known her by is Black Widow.", "Black Widow");
-makeClozeCard("Superman is weak against kryptonite.","Superman");
+makeClozeCard("Superman is weak against kryptonite.", "Superman");
 
 //testing for errors
-console.log(clozeQuestion);
-
-// keep tack of current question and score wronge and right answers
-var currentBasicQuestion = 0;
-var currentClozeQuestion = 0;
-var correct = 0;
-var incorrect = 0;
-var totalQuestion = 0;
+//console.log(clozeQuestion);
 
 function start() {
-
+    //overall total amount of question. if there are still question left run 
     if (basicQuestion.length + clozeQuestion.length > totalQuestion) {
-        //since there are only 3 basic questions
+        //see if there are any basic question left
         if (basicQuestion.length > totalQuestion) {
             inquirer.prompt([
                 {
@@ -61,20 +59,20 @@ function start() {
                     incorrect++;
                     console.log("WRONG");
                 }
-            //add to question so it goes to the next one 
+                //add to question so it goes to the next one 
                 currentBasicQuestion++;
-            //add to total questions to see ther are any left
+                //add to total questions to see ther are any left
                 totalQuestion++;
-                start();//run function again to get next question 
+                start(); //run function again to get next question 
             });
-            //end of basic quesion
-        }else{
-        // now the 3 cloze question
-                  inquirer.prompt([
+            //end of basic quesion. There are no more basic questions
+        } else {
+            // now the 3 cloze question
+            inquirer.prompt([
                 {
                     name: "name",
                     message: clozeQuestion[currentClozeQuestion].partial
-                    
+
   }]).then(function (answer) {
                 //see if the answer wrong or right and add to score and to the currentQuestion.
                 if (answer.name.toLowerCase() === clozeQuestion[currentClozeQuestion].cloze.toLowerCase()) {
@@ -85,13 +83,39 @@ function start() {
                     console.log("WRONG")
                 }
                 //add to total questions to see ther are any left
-               totalQuestion++;
-               currentClozeQuestion++;  
-               start();//run function again to get next question       
+                totalQuestion++;
+                currentClozeQuestion++;
+                start(); //run function again to get next question       
             });
-}//end of cloze questions
-        
-//basic + cloze else{}
-    } 
+        } //end of cloze questions
+
+        //basic + cloze end. Now end game.
+    } else {
+        console.log("You are finish. Here is your score:" + "\n Correct: " + correct + "\n Wrong:" + incorrect);
+        inquirer
+            .prompt([
+            // Here we ask the user to confirm.
+                {
+                    type: "confirm",
+                    message: "Do you want to go for another round?",
+                    name: "confirm",
+                    default: true
+    }
+     ]).then(function (inquirerResponse) {
+                //user wants to play again
+                if (inquirerResponse.confirm) {
+                    //resetting the vaules for playing again
+                    currentBasicQuestion = 0;
+                    currentClozeQuestion = 0;
+                    correct = 0;
+                    incorrect = 0;
+                    totalQuestion = 0;
+                    start();
+                } else {
+                    console.log("Okay come back another time.");
+                }
+            });
+
+    }
 } //function start end
 start();
